8060d170bd876c4bc7db3359e360d34235aab0e7
_ <_>
2026-05-06 11:38:05 +0300

    adding images

---
+++ website/pages/hard.ts
@@ -0,0 +1,16 @@
+import { get } from "componentUtilities";
+
+var stopperDiv = get("div", "stopperDiv");
+var stopperButton = get("button", "stopperButton");
+
+var seconds = 0;
+
+stopperButton.onclick = function () {
+    setInterval(
+        function () {
+            seconds++;
+            stopperDiv.innerText = String(seconds);
+        },
+        1000
+    );
+};
