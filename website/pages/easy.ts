bda65a1b60e4802cad67f32510e6fb5e99b61c4d
_ <_>
2026-05-14 14:29:17 +0300

    nothing specual

--- website/pages/easy.ts
+++ website/pages/easy.ts
@@ -3,26 +3,28 @@ import { create, get } from "componentUtilities";
 var stopperDiv = get("div", "stopperDiv");
 var stopperButton = get("button", "stopperButton");
 var images1to5Div = get("div", "images1-5");
+var images6to10Div = get("div", "images6-10");
 
 var seconds = 0;
 
-for (let i = 0; i < 5; i++) {
+for (let j = 0; j < 5; j++) {
     let cardImg = create("img", { className: "cardImg", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlXY0YRlBOQZUyNw3Rz49lQSWrIVPkmmTcPg&s" });
     cardImg.onclick = function () {
         cardImg.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/01_of_spades_A.svg/960px-01_of_spades_A.svg.png";
     }
     images1to5Div.append(cardImg);
 }
+for (let a = 6; a < 10; a++) {
+    let cardImg2 = create("img", {
+        className: "cardImg2",
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlXY0YRlBOQZUyNw3Rz49lQSWrIVPkmmTcPg&s"
+    });
 
+    cardImg2.onclick = function () {
+        cardImg2.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/01_of_spades_A.svg/960px-01_of_spades_A.svg.png";
+    }
 
-stopperButton.onclick = function () {
-    setInterval(
-        function () {
-            seconds++;
-            stopperDiv.innerText = String(seconds);
-        },
-        1000
-    );
-};
+    images6to10Div.append(cardImg2);
+}
 
 
